862f51ac3e469c8816db4efa3de8abe30c34fa26
JVGS1111 <joaoviniciusgs@gmail.com>
2021-11-27 22:31:32 -0300

    initial commit

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text } from '@chakra-ui/react'
+
+export function Logo() {
+    return (
+        <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+            Dashgo
+            <Text as="span" ml="1" color="pink.500">.</Text>
+        </Text>
+    )
+}
